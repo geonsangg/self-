43d78cfdaf83f4c89be8b133df113f8f2887dda1
rjstk <rjstk@LAPTOP-5L1DN0AC>
2022-12-06 14:31:19 +0900

    주석 사용해보기

--- chap01/src/exam01/hello.java
+++ chap01/src/exam01/hello.java
@@ -1,10 +1,17 @@
 package exam01;
 
+/*
+ 작성자:이건상
+ 작성일:2022-12-06
+ */
+
 public class hello {
 
+	//프로그램 실행 진입점
 	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
 		System.out.println("Hello, Java");
 
-	}
+	}//end of main
 
-}
+}//end of class
