37999246c385aeddda554669192c0baddb729466
rjstk <rjstk@LAPTOP-5L1DN0AC>
2022-12-07 17:19:46 +0900

    boolean 사용해보기

---
+++ chap01/src/chap02/exam04/BooleanExample.java
@@ -0,0 +1,16 @@
+package chap02.exam04;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+
+		boolean stop = false;
+		
+		if(stop) {
+			System.out.println("중지");
+		} else {
+			System.out.println("시작");
+		}
+	}
+
+}
