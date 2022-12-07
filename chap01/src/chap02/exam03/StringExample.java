c960915f1f0f7c96dd113d855cf98fe557e87171
rjstk <rjstk@LAPTOP-5L1DN0AC>
2022-12-07 16:37:29 +0900

    string 사용해보기

---
+++ chap01/src/chap02/exam03/StringExample.java
@@ -0,0 +1,15 @@
+package chap02.exam03;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+
+		String name = "이건상";
+		String job = "프로그래머";
+		
+		System.out.println(name + job);
+		
+
+	}
+
+}
