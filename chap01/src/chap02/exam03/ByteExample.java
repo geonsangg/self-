ee136126e04b72656f30a47a242a01638ba07d54
rjstk <rjstk@LAPTOP-5L1DN0AC>
2022-12-07 16:13:09 +0900

    byte타입 사용해보기

---
+++ chap01/src/chap02/exam03/ByteExample.java
@@ -0,0 +1,16 @@
+package chap02.exam03;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+
+		byte var1 = -128;
+		byte var2 = 0;
+		byte var3 = 127;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
