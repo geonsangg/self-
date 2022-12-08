e169eb773a085133981db5ddb7c811883007bab6
geonsangg <rjstkdd@naver.com>
2022-12-08 15:56:15 +0900

    long 정수연산 자동변환 사용해보기

---
+++ chap01/src/chap03/exam02/LongOperationExample.java
@@ -0,0 +1,17 @@
+package chap03.exam02;
+
+public class LongOperationExample {
+
+	public static void main(String[] args) {
+
+
+		byte value1 = 10;
+		int value2 = 100;
+		long value3 = 1000;
+		
+		long resurt = value1 + value2 + value3;
+		System.out.println(resurt);
+
+	}
+
+}
