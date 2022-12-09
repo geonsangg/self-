1090bbd08516dcadcf3be0db40e2b029880c99fc
geonsangg <rjstkdd@naver.com>
2022-12-09 13:54:50 +0900

    연산식에서 자동타입변환 사용해보기

---
+++ chap01/src/chap03/exam02/OperationPromotionExample.java
@@ -0,0 +1,34 @@
+package chap03.exam02;
+
+public class OperationPromotionExample {
+
+	public static void main(String[] args) {
+
+		byte byteValue1 = 10;
+		byte byteValue2 = 20;
+		//byte byteValue3 = byteValue1 + byteValue2 (정수 연산은 int로 바뀜)
+		int intValue1 = byteValue1 + byteValue2;
+		System.out.println(intValue1);
+		
+		char charValue1 = 'A';
+		char charValue2 = 1;
+		//char도 문자가 유니코드로 바뀌어서 정수연산 됌
+		int intValue2 = charValue1 + charValue2;
+		System.out.println("유니코드=" + intValue2);
+		System.out.println("출력문자=" + (char)intValue2);
+		
+		int intValue3 = 10;
+		int intValue4 = intValue3 / 4;  //10 / 4 = 2.5 (정수연산이라 소수점은 없어짐)
+		System.out.println(intValue4);
+		double doubleValue = intValue3 / 4.0;
+		System.out.println(doubleValue);
+		
+		int x = 1;
+		int y = 2;
+		double result = (double) x / y;
+		System.out.println(result);
+				
+
+	}
+
+}
