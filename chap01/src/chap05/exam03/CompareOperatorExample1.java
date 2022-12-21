107f9cc9d6af46a933cc9866b85d2a031571a4ee
geonsangg <rjstkdd@naver.com>
2022-12-21 12:29:55 +0900

    비교연산 해보기 2

--- chap01/src/chap05/exam03/CompareOperatorExample1.java
+++ chap01/src/chap05/exam03/CompareOperatorExample1.java
@@ -3,20 +3,19 @@ package chap05.exam03;
 public class CompareOperatorExample1 {
 
 	public static void main(String[] args) {
-		int num1 = 10;
-		int num2 = 10;
-		boolean result1 = (num1 == num2);
-		boolean result2 = (num1 != num2);
-		boolean result3 = (num1 <= num2);
-		System.out.println("result1= " + result1);
-		System.out.println("result2= " + result2);
-		System.out.println("result3= " + result3);
+		int v2 = 1;
+		double v3 = 1.0;
+		System.out.println(v2 == v3);
+		//int -> double 로 바뀌어서 연산되어서 true
 		
-		char char1 = 'A';
-		char char2 = 'B';
-		boolean result4 = (char1 < char2);
-		System.out.println("result4= " + result4);
-		
+		double v4 = 0.1;
+		float v5 = 0.1f;
+		System.out.println((double)v5);
+		System.out.println(v4 == v5);
+		System.out.println((float)v4 == v5);
+		System.out.println((int)(v4*10) == (int)(v5*10));
+		//float을 double로 강제타입변환하면 부동소수점 방식때문에 정확한 0.1이 안나옴.
+		//float과 double을 비교할때에는 float으로 바꾸고 비교 or 정수로 바꿔서 비교
 	}
 
 }
